dedc9f9b462d1245e2b601b66987e03e0a76cd17
Ryan Dick <rdick@ualberta.ca>
2020-06-22 19:08:42 -0700

    added new note page

---
+++ pages/[id]/index.js
@@ -0,0 +1,3 @@
+import fetch from 'isomorphic-unfetch'
+import { useState, useEffect } from 'react'
+import 
--- pages/new.js
+++ pages/new.js
@@ -1,31 +1,47 @@
-import Link from 'next/link'
-import { useState, useEffect, createContext } from 'react'
-import { fetch } from 'isomorphic-unfetch'
-import { Button, Form, Loader } from 'semantic-ui-react'
-import { useRouter } from 'next/router'
+import Link from 'next/link';
+import { useState, useEffect } from 'react';
+import fetch from 'isomorphic-unfetch';
+import { Button, Form, Loader } from 'semantic-ui-react';
+import { useRouter } from 'next/router';
 
 const NewNote = () => {
-    const [form, setForm] = useState({ title: '', description: '' })
-    const [isSubmiting, setIsSubmitting] = useState(false)
-    const [errors, setErrors] = useState({})
-    const router = useRouter()
+    const [form, setForm] = useState({ title: '', description: '' });
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [errors, setErrors] = useState({});
+    const router = useRouter();
 
     useEffect(() => {
-        if (isSubmiting) {
+        if (isSubmitting) {
             if (Object.keys(errors).length === 0) {
-                // createNote()
-                alert('Success')
-            } else {
-                setIsSubmitting(false)
+                createNote();
+            }
+            else {
+                setIsSubmitting(false);
             }
         }
     }, [errors])
 
+    const createNote = async () => {
+        try {
+            const res = await fetch('http://localhost:3000/api/notes', {
+                method: 'POST',
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(form)
+            })
+            router.push("/");
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     const handleSubmit = (e) => {
-        e.preventDefault()
-        let errs = validate()
-        setErrors(errors)
-        setIsSubmitting(true)
+        e.preventDefault();
+        let errs = validate();
+        setErrors(errs);
+        setIsSubmitting(true);
     }
 
     const handleChange = (e) => {
@@ -36,46 +52,43 @@ const NewNote = () => {
     }
 
     const validate = () => {
-        let err = {}
+        let err = {};
 
         if (!form.title) {
-            err.title = 'Title is required'
+            err.title = 'Title is required';
         }
         if (!form.description) {
-            err.description = 'Description is required'
+            err.description = 'Description is required';
         }
 
-        return err
+        return err;
     }
 
     return (
-        <div className='form-container'>
+        <div className="form-container">
             <h1>Create Note</h1>
             <div>
                 {
-                    isSubmiting
+                    isSubmitting
                         ? <Loader active inline='centered' />
                         : <Form onSubmit={handleSubmit}>
                             <Form.Input
                                 fluid
-                                error={errors.title ? { content: 'Please Enter a Title', pointing: 'below' } : null}
-                                label="Title"
+                                error={errors.title ? { content: 'Please enter a title', pointing: 'below' } : null}
+                                label='Title'
                                 placeholder='Title'
                                 name='title'
                                 onChange={handleChange}
                             />
                             <Form.TextArea
                                 fluid
-                                error={errors.description ? { content: 'Please Enter a Description', pointing: 'below' } : null}
-                                label='Description'
+                                label='Descriprtion'
                                 placeholder='Description'
-                                name='Description'
+                                name='description'
+                                error={errors.description ? { content: 'Please enter a description', pointing: 'below' } : null}
                                 onChange={handleChange}
                             />
                             <Button type='submit'>Create</Button>
-
-
-
                         </Form>
                 }
             </div>
@@ -83,4 +96,4 @@ const NewNote = () => {
     )
 }
 
-export default NewNote
+export default NewNote;
