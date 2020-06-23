ede12cc08857d74e7bf3c73b220cab14156bd591
Ryan Dick <rdick@ualberta.ca>
2020-06-22 20:11:11 -0700

    created show page with delete button

--- pages/[id]/index.js
+++ pages/[id]/index.js
@@ -1,3 +1,68 @@
 import fetch from 'isomorphic-unfetch'
 import { useState, useEffect } from 'react'
-import 
+import { useRouter } from 'next/router'
+import { Confirm, Button, Loader } from 'semantic-ui-react'
+
+const Note = ({ note }) => {
+    const [confirm, setConfirm] = useState(false)
+    const [isDeleting, setIsDeleting] = useState(false)
+    const router = useRouter()
+
+    useEffect(() => {
+        if (isDeleting) {
+            deleteNote()
+        }
+    }, [isDeleting])
+
+    const deleteNote = async () => {
+        const noteId = router.query.id
+        try {
+            const deleted = await fetch(`http://localhost:3000/api/notes/${noteId}`, {
+                method: "DELETE"
+            })
+            router.push("/")
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const open = () => setConfirm(true);
+
+    const close = () => setConfirm(false);
+
+    const handleDelete = async () => {
+        setIsDeleting(true);
+        close();
+    }
+
+
+    return (
+        <div className='note-container'>
+            {isDeleting
+                ? <Loader active />
+                :
+                <>
+                    <h1>{note.title}</h1>
+                    <p>{note.description}</p>
+                    <Button color='red' onClick={open}>
+                        Delete
+                    </Button>
+                </>
+            }
+            <Confirm
+                open={confirm}
+                onCancel={close}
+                onConfirm={handleDelete}
+            />
+        </div>
+    )
+}
+
+Note.getInitialProps = async ({ query: { id } }) => {
+    const res = await fetch(`http://localhost:3000/api/notes/${id}`)
+    const { data } = await res.json()
+
+    return { note: data }
+}
+
+export default Note
